7d51820bd26412b3e0a4c6b2b7404cca399a81c8
Xavier BURKE <xavier.burke@gmail.com>
2021-04-21 17:15:00 +0200

    Add PublicController

---
+++ src/Controller/Webapp/PublicController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Webapp;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PublicController extends AbstractController
+{
+    /**
+     * @Route("/webapp/public", name="webapp_public")
+     */
+    public function index(): Response
+    {
+        return $this->render('webapp/public/index.html.twig', [
+            'controller_name' => 'PublicController',
+        ]);
+    }
+}

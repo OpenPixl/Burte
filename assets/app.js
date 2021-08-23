251b56417d94376961ae6f8440d6321bc514c7ec
Xavier BURKE <xavier.burke@openpixl.fr>
2021-08-23 20:48:42 +0200

    integration js landkit

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,7 @@
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
 require('bootstrap');
+import './styles/js/theme';
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 // start the Stimulus application
 import './bootstrap';
